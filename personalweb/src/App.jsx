54fcb890b2f92fb88b8f44abf8ede89eb91ea5f7
GitWorkingTime <113561439+GitWorkingTime@users.noreply.github.com>
2025-10-10 15:48:52 -0400

    Interactive Buttons

--- personalweb/src/App.jsx
+++ personalweb/src/App.jsx
@@ -3,6 +3,7 @@ import darkModeButton from './assets/webp/icons8-dark-mode-50.webp'
 import githubIcon from './assets/webp/github-mark.webp'
 import linkedInIcon from './assets/webp/InBug-Black.webp'
 import itchIcon from './assets/webp/itchio-logo-textless-black.webp'
+import resume from './assets/Resume_External.pdf'
 
 function App() {
   return (
@@ -14,7 +15,7 @@ function App() {
       </div>
       <div className="navbar-right">
         <button className='links-contact links-button links-light'>Contact Me</button>
-        <img className='dark-mode-button' src={darkModeButton} height={30} width={30}></img>
+        <img className='dark-mode-button links-button' src={darkModeButton} height={30} width={30}></img>
       </div>
     </div>
     <div className="home">
@@ -28,20 +29,34 @@ function App() {
       </div>
 
       <div className="links">
-        <button className='links-resume links-button links-dark'>Download Resume</button>
-        <button className='links-github links-button'>
-          <img src={githubIcon} height={40} width={40} alt="GitHub Icon"></img>
-        </button>
-        <button className='links-linkedin links-button'>
-          <img src={linkedInIcon} height={40} width={40} alt="LinkedIn Icon"></img>
-        </button>
-        <button className='links-itch links-button'>
-          <img src={itchIcon} height={40} width={40} alt="Itch.io Icon"></img>
-        </button>
+        <a href={resume} download="Resume" target="_blank">
+         <button className='links-resume links-button links-dark'>Download Resume</button>
+        </a>
+        <a href="https://github.com/GitWorkingTime" target="_blank" rel="noopener noreferrer">
+          <button className='links-github links-button'>
+            <img src={githubIcon} height={40} width={40} alt="GitHub Icon"></img>
+          </button>
+        </a>
+        <a href="https://www.linkedin.com/in/daniel-s-l/" target="_blank" rel="noopener noreferrer">
+          <button className='links-linkedin links-button'>
+            <img src={linkedInIcon} height={40} width={40} alt="LinkedIn Icon"></img>
+          </button>
+        </a>
+        <a href="https://doopsgame.itch.io/" target="_blank" rel="noopener noreferrer">
+          <button className='links-itch links-button'>
+            <img src={itchIcon} height={40} width={40} alt="Itch.io Icon"></img>
+          </button>
+        </a>
       </div>
     </div>
     </>
   )
+}
+
+function Download(){
+  const downloadFile = () => {
+
+  }
 }
 
 export default App
